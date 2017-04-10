bc0ba8dca15241b14a93c434f5a0b80dfac4fe08
maybe_code <maybe_code@sina.com>
2017-04-10 14:36:25 +0800

    图文版的新闻客户端

--- app/src/main/java/uianimationdemo/com/example/daysnews/adapter/MyRecyclerViewAdapter.java
+++ app/src/main/java/uianimationdemo/com/example/daysnews/adapter/MyRecyclerViewAdapter.java
@@ -3,14 +3,12 @@ package uianimationdemo.com.example.daysnews.adapter;
 import android.content.Context;
 import android.graphics.Color;
 import android.support.v7.widget.RecyclerView;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 import java.util.ArrayList;
 import java.util.List;
-
 import uianimationdemo.com.example.daysnews.R;
 import uianimationdemo.com.example.daysnews.entiy.NewsListInfo;
 
